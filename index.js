cfda530213fe5d49902cc00aa6b59a6154b859a2
Jacob <perezjacob2@gmail.com>
2020-12-30 17:20:44 -0600

    3D Menu with effect

---
+++ index.js
@@ -0,0 +1,16 @@
+var button = document.getElementById("btn");
+var container = document.getElementById("container");
+var menu = document.getElementById("menu")
+var close = true;
+button.addEventListener("click" ,function() {
+    container.classList.add("active");
+    if(close){
+        container.classList.add("active");
+        menu.style.zIndex = "0";
+        close = false
+    }else{
+        close = true;
+        container.classList.remove("active");
+        menu.style.zIndex = "-1";
+    }
+});
